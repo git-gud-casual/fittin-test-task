da4e00defdb8a2673a5f5a283b9e4c6c149a3f90
admin <popasena2@ya.ru>
2024-07-19 06:56:47 +0300

    Add tests for product retrieve and categories

--- app/products/tests.py
+++ app/products/tests.py
@@ -1,3 +1,46 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase, APIClient
+from rest_framework import status
+from json import loads
 
-# Create your tests here.
+from .models import Product, Category
+from .serializers import CategorySerializer, ProductSerializer
+
+
+class ProductsTestCase(APITestCase):
+    fixtures = ["categories.json"]
+
+    api_client: APIClient
+
+    @classmethod
+    def setUpTestData(cls):
+        product = Product()
+        product.name = "test"
+        product.price = 1
+        product.image = ("https://cdn.britannica.com/70"
+                         "/234870-050-D4D024BB/"
+                         "Orange-colored-cat-yawns-displaying-teeth.jpg")
+        product.description = "test"
+        product.category = Category.objects.get(pk=1)
+        product.save()
+
+        cls.api_client = APIClient()
+
+    def test_retrieve(self):
+        resp = loads(self.api_client.get("/product/1/").content)
+        ser1 = ProductSerializer(Product.objects.get(pk=1))
+        self.assertDictEqual(ser1.data, resp)
+
+    def test_retrieve404(self):
+        for id in (-1, 0, 2, 3):
+            code = self.api_client.get(f"/product/{id}/").status_code
+            self.assertEqual(status.HTTP_404_NOT_FOUND, code)
+
+
+class CategoriesTestCase(APITestCase):
+    fixtures = ["categories.json"]
+    api_client = APIClient()
+
+    def test_get_categories(self):
+        categories = CategorySerializer(Category.objects.filter(parent_category_id=None).all(), many=True).data
+        resp = loads(self.api_client.get("/categories").content)
+        self.assertCountEqual(resp, categories)
--- app/products/urls.py
+++ app/products/urls.py
@@ -3,6 +3,6 @@ from django.urls import path, re_path
 from .views import CategoryListView, ProductRetrieveView
 
 urlpatterns = [
-    path("category", CategoryListView.as_view()),
+    path("categories", CategoryListView.as_view()),
     re_path("^product/(?P<pk>.+)/$", ProductRetrieveView.as_view())
 ]
