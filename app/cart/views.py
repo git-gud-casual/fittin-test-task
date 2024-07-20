18199954e03a45d9416ef1aaa3194228173ee635
admin <popasena2@ya.ru>
2024-07-20 09:51:36 +0300

    Delete unused imports

--- app/cart/views.py
+++ app/cart/views.py
@@ -1,6 +1,4 @@
 from rest_framework import generics, mixins, viewsets
-from rest_framework.response import Response
-from rest_framework.decorators import action
 from rest_framework.permissions import IsAuthenticated
 from rest_framework_simplejwt.authentication import JWTAuthentication
 from django.shortcuts import get_object_or_404
