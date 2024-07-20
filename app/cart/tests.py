d8fddb35e8df81aa8402c61cbb975c0cb1a20b95
admin <popasena2@ya.ru>
2024-07-20 11:20:15 +0300

    Add tests for cart

--- app/cart/tests.py
+++ app/cart/tests.py
@@ -1,3 +1,161 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase, APIClient
+from rest_framework_simplejwt.tokens import RefreshToken
+from django.contrib.auth.models import User
+from rest_framework.exceptions import ValidationError
+from rest_framework import status
+from json import loads
 
-# Create your tests here.
+from products.models import Product, ProductSize, Category
+from .models import CartEntry
+
+
+class ProductsTestCase(APITestCase):
+    fixtures = ["categories.json"]
+    api_client: APIClient
+
+    def setUp(self):
+        self.user = User.objects.create_user(username="test",
+                                        password="test")
+        token = RefreshToken.for_user(self.user)
+        self.api_client = APIClient()
+        self.api_client.credentials(HTTP_AUTHORIZATION=f"Bearer {token.access_token}")
+        product1 = Product()
+        product1.name = "test"
+        product1.price = 1000
+        product1.image = ("https://cdn.britannica.com/70"
+                          "/234870-050-D4D024BB/"
+                          "Orange-colored-cat-yawns-displaying-teeth.jpg")
+        product1.description = "test"
+        product1.category = Category.objects.get(id=1)
+        product1.save()
+
+        product2 = Product()
+        product2.name = "test2"
+        product2.price = 1500
+        product2.image = ("https://cdn.britannica.com/70"
+                          "/234870-050-D4D024BB/"
+                          "Orange-colored-cat-yawns-displaying-teeth.jpg")
+        product2.description = "test"
+        product2.category = Category.objects.get(id=3)
+        product2.save()
+
+        self.product1 = product1
+        self.product2 = product2
+
+        self.sizes = ProductSize.objects.bulk_create((
+            ProductSize(size="s", product=product1, count_in_stock=100),
+            ProductSize(size="m", product=product1, count_in_stock=100),
+            ProductSize(size="l", product=product1, count_in_stock=100),
+            ProductSize(size="m", product=product2, count_in_stock=100),
+            ProductSize(size="l", product=product2, count_in_stock=100)
+        ))
+
+        CartEntry.objects.bulk_create((
+            CartEntry(product=self.sizes[0], cart=self.user.cart, count=1),
+            CartEntry(product=self.sizes[-1], cart=self.user.cart, count=5),
+        ))
+
+    def test_get1(self):
+        value = [
+            {
+                "product_id": self.product1.id,
+                "size": "s",
+                "count": 1
+            },
+            {
+                "product_id": self.product2.id,
+                "size": "l",
+                "count": 5
+            },
+        ]
+        resp = loads(self.api_client.get("/cart").content)
+        self.assertCountEqual(value, resp)
+
+    def test_get2(self):
+        self.user.cart.products.clear()
+        value = []
+        resp = loads(self.api_client.get("/cart").content)
+        self.assertCountEqual(value, resp)
+
+    def test_post1(self):
+        size = self.sizes[1]
+        body = {
+            "product_id": size.product.id,
+            "size": size.size,
+            "count": 1
+        }
+        code = self.api_client.post("/cart", body, format="json").status_code
+        self.assertEqual(status.HTTP_201_CREATED, code)
+        new_entry = self.user.cart.products.through.objects.get(product=size)
+        self.assertEqual(new_entry.product.product_id, body["product_id"])
+        self.assertEqual(new_entry.product.size, body["size"])
+        self.assertEqual(new_entry.count, body["count"])
+
+    def test_post2(self):
+        size = self.sizes[1]
+        body = {
+            "product_id": size.product.id,
+            "size": size.size,
+            "count": 0
+        }
+        code = self.api_client.post("/cart", body, format="json").status_code
+        self.assertEqual(status.HTTP_400_BAD_REQUEST, code)
+        body = {
+            "product_id": size.product.id,
+            "size": size.size,
+            "count": -1
+        }
+        code = self.api_client.post("/cart", body, format="json").status_code
+        self.assertEqual(status.HTTP_400_BAD_REQUEST, code)
+
+    def test_post3(self):
+        size = self.sizes[0]
+        body = {
+            "product_id": size.product.id,
+            "size": size.size,
+            "count": 1
+        }
+        code = self.api_client.post("/cart", body, format="json").status_code
+        self.assertEqual(status.HTTP_201_CREATED, code)
+        entry = self.user.cart.products.through.objects.get(product=size)
+        self.assertEqual(entry.count, 2)
+
+    def test_post4(self):
+        body = {
+            "product_id": self.product2.id,
+            "size": "s",
+            "count": 1
+        }
+        code = self.api_client.post("/cart", body, format="json").status_code
+        self.assertEqual(status.HTTP_400_BAD_REQUEST, code)
+
+    def test_put(self):
+        size = self.sizes[0]
+        body = {
+            "product_id": size.product_id,
+            "size": size.size,
+            "count": 10
+        }
+        code = self.api_client.put(f"/cart/{size.product_id}/{size.size}",
+                                   body, format="json").status_code
+        self.assertEqual(status.HTTP_200_OK, code)
+        entry = self.user.cart.products.through.objects.get(product=size)
+        self.assertEqual(entry.count, 10)
+
+    def test_delete(self):
+        size = self.sizes[0]
+        code = self.api_client.delete(f"/cart/{size.product_id}/{size.size}").status_code
+        self.assertEqual(status.HTTP_204_NO_CONTENT, code)
+        with self.assertRaises(CartEntry.DoesNotExist):
+            _ = self.user.cart.products.through.objects.get(product=size)
+
+    def test_unauthorized(self):
+        api_client = APIClient()
+        code = api_client.get("/cart").status_code
+        self.assertEqual(status.HTTP_401_UNAUTHORIZED, code)
+        code = api_client.put("/cart/1/m").status_code
+        self.assertEqual(status.HTTP_401_UNAUTHORIZED, code)
+        code = api_client.post("/cart").status_code
+        self.assertEqual(status.HTTP_401_UNAUTHORIZED, code)
+        code = api_client.delete("/cart/1/m").status_code
+        self.assertEqual(status.HTTP_401_UNAUTHORIZED, code)
