324a47adb27c2cd078336d0ab92b7e7103526a2b
admin <popasena2@ya.ru>
2024-07-20 14:21:02 +0300

    Add order in admin

--- app/cart/admin.py
+++ app/cart/admin.py
@@ -1,8 +1,24 @@
 from django.contrib import admin
 
-from .models import CartEntry
+from .models import CartEntry, Order, OrderEntry
 
 
 @admin.register(CartEntry)
 class CartEntryAdmin(admin.ModelAdmin):
     list_display = ["cart", "product", "count"]
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ["user", "created_at", "payed", "final_price"]
+
+    def picture_image(self, obj: Order):
+        return obj.final_price
+
+    def has_add_permission(self, request):
+        return False
+
+
+@admin.register(OrderEntry)
+class OrderEntryAdmin(admin.ModelAdmin):
+    list_display = ["order", "product", "count", "final_price"]
