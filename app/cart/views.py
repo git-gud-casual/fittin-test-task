9952fbded190156ada179cbc44e333cd8c1d6b33
admin <popasena2@ya.ru>
2024-07-20 11:20:07 +0300

    Deleted unused code

--- app/cart/views.py
+++ app/cart/views.py
@@ -1,37 +1,11 @@
-from rest_framework import generics, mixins, viewsets
+from rest_framework import mixins, viewsets
 from rest_framework.permissions import IsAuthenticated
 from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework.exceptions import ValidationError
 from django.shortcuts import get_object_or_404
 
 from .serializers import CartEntrySerializer
 from .models import CartEntry, ProductSize
-
-
-class CartListCreateView(generics.ListCreateAPIView):
-    serializer_class = CartEntrySerializer
-    permission_classes = [IsAuthenticated]
-    authentication_classes = [JWTAuthentication]
-
-    def get_queryset(self):
-        if self.request.user.is_anonymous:
-            return
-        return CartEntry.objects.filter(cart=self.request.user.cart).all()
-
-    def perform_create(self, serializer):
-        data = serializer.validated_data
-        product = get_object_or_404(ProductSize, product_id=data["product"]["product_id"],
-                                    size=data["product"]["size"])
-        try:
-            cart_entry = CartEntry.objects.get(
-                cart=self.request.user.cart,
-                product=product
-            )
-            cart_entry.count += data["count"]
-            cart_entry.save()
-        except CartEntry.DoesNotExist:
-            CartEntry.objects.create(cart=self.request.user.cart,
-                                     product=product,
-                                     count=data["count"])
 
 
 class CartViewSet(viewsets.GenericViewSet,
@@ -39,7 +13,6 @@ class CartViewSet(viewsets.GenericViewSet,
                   mixins.CreateModelMixin,
                   mixins.DestroyModelMixin,
                   mixins.UpdateModelMixin):
-
     serializer_class = CartEntrySerializer
     permission_classes = [IsAuthenticated]
     authentication_classes = [JWTAuthentication]
@@ -55,28 +28,34 @@ class CartViewSet(viewsets.GenericViewSet,
 
     def perform_create(self, serializer):
         data = serializer.validated_data
-        product = get_object_or_404(ProductSize, product_id=data["product"]["product_id"],
-                                    size=data["product"]["size"])
         try:
-            cart_entry = CartEntry.objects.get(
-                cart=self.request.user.cart,
-                product=product
-            )
-            cart_entry.count += data["count"]
-            cart_entry.save()
-        except CartEntry.DoesNotExist:
-            CartEntry.objects.create(cart=self.request.user.cart,
-                                     product=product,
-                                     count=data["count"])
+            product = ProductSize.objects.get(product_id=data["product"]["product_id"],
+                                              size=data["product"]["size"])
+            try:
+                cart_entry = CartEntry.objects.get(
+                    cart=self.request.user.cart,
+                    product=product
+                )
+                cart_entry.count += data["count"]
+                cart_entry.save()
+            except CartEntry.DoesNotExist:
+                CartEntry.objects.create(cart=self.request.user.cart,
+                                         product=product,
+                                         count=data["count"])
+        except ProductSize.DoesNotExist:
+            raise ValidationError({"message": "productsize does not exist"})
 
     def perform_update(self, serializer):
         data = serializer.validated_data
-        product = get_object_or_404(ProductSize, product_id=data["product"]["product_id"],
-                                    size=data["product"]["size"])
-        cart_entry = self.get_object()
-        cart_entry.product = product
-        cart_entry.count = data["count"]
-        cart_entry.save()
+        try:
+            product = ProductSize.objects.get(product_id=data["product"]["product_id"],
+                                              size=data["product"]["size"])
+            cart_entry = self.get_object()
+            cart_entry.product = product
+            cart_entry.count = data["count"]
+            cart_entry.save()
+        except ProductSize.DoesNotExist:
+            raise ValidationError({"message": "productsize does not exist"})
 
     def get_object(self):
         queryset = self.filter_queryset(self.get_queryset())
